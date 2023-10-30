5989e7dca2f944ac25116dedb8c0dc89f6030432
Filip Macak <filip.macak1999@gmail.com>
2023-10-30 19:33:12 +0100

    Added asserts to pomdp tests

--- tests/pomdp/test_pomdp_quantitative_analysis.py
+++ tests/pomdp/test_pomdp_quantitative_analysis.py
@@ -18,10 +18,10 @@ class TestPomdpQuantitative:
         options.use_clipping = False
         belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options)
         result = belmc.check(formulas[0].raw_formula, [])
-        print(result.lower_bound)
-        print(result.upper_bound)
-        print(result.induced_mc_from_scheduler)
-        print(result.cutoff_schedulers[0])
+        assert round(result.lower_bound, 3) == 0.352
+        assert result.induced_mc_from_scheduler.nr_states == 10
+        assert result.induced_mc_from_scheduler.nr_transitions == 23
+        assert result.cutoff_schedulers[0].get_choice(1).__str__() == "{[0.25: 0], [0.25: 1], [0.25: 2], [0.25: 3], }"
 
     def test_underapprox_mc_maze_rmin(self):
         program = stormpy.parse_prism_program(get_example_path("pomdp", "maze_2.prism"))
@@ -34,10 +34,10 @@ class TestPomdpQuantitative:
         options.use_clipping = False
         belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options)
         result = belmc.check(formulas[0].raw_formula, [])
-        print(result.lower_bound)
-        print(result.upper_bound)
-        print(result.induced_mc_from_scheduler)
-        print(result.cutoff_schedulers[0])
+        assert round(result.upper_bound, 3) == 12.270
+        assert result.induced_mc_from_scheduler.nr_states == 9
+        assert result.induced_mc_from_scheduler.nr_transitions == 17
+        assert result.cutoff_schedulers[0].get_choice(2).__str__() == "{[0.5: 0], [0.5: 1], }"
 
     def test_underapprox_mc_cmaze_rmin(self):
         program = stormpy.parse_prism_program(get_example_path("pomdp", "maze-concise.prism"))
@@ -50,7 +50,7 @@ class TestPomdpQuantitative:
         options.use_clipping = False
         belmc = stormpy.pomdp.BeliefExplorationModelCheckerDouble(model, options)
         result = belmc.check(formulas[0].raw_formula, [])
-        print(result.lower_bound)
-        print(result.upper_bound)
-        print(result.induced_mc_from_scheduler)
-        print(result.cutoff_schedulers[0])
+        assert round(result.upper_bound, 3) == 19.781
+        assert result.induced_mc_from_scheduler.nr_states == 9
+        assert result.induced_mc_from_scheduler.nr_transitions == 15
+        assert result.cutoff_schedulers[0].get_choice(5).__str__() == "{[0.5: 1], [0.5: 3], }"
